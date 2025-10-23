3204f9826b10b371a18d2c34dc098bf84d54d945
guoliveira0 <luizgustavo160206@gmail.com>
2025-10-23 08:58:48 -0300

    Comecei a criar a classe usuário

--- Usuario.cs
+++ Usuario.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Trabalho1_ProgVis
 {
-    internal class Usuario
+    [Table("Usuarios")]
+    public class Usuario
     {
+        [Key]
+        public Int64  Id        { get; set; }
+        [Required]
+        [StringLength(45)]
+        public String? Nome     { get; set; }
+        public String? Telefone { get; set; }
+        public String? Email    { get; set; }
     }
 }
